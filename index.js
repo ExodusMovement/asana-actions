0219e6b7fc4f8218b2d8018e5e514396edc01776
maxogden <max@maxogden.com>
2021-05-10 21:08:54 -0700

    1.0.10

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const asana = require('./asana')
 
-try {
-  const ASANA_TOKEN = core.getInput('token')
-  const WORKSPACE = core.getInput('workspace')
-  const PR = github.context.payload.pull_request
+const run = async () => {
+  try {
+    const ASANA_TOKEN = core.getInput('token')
+    const WORKSPACE = core.getInput('workspace')
+    const PR = github.context.payload.pull_request
 
-  if (!ASANA_TOKEN){
-    throw({message: 'ASANA_TOKEN not set'});
+    if (!ASANA_TOKEN){
+      throw({message: 'ASANA_TOKEN not set'})
+    }
+    
+    const shortId = asana.getAsanaShortId(PR.title)
+    if (!shortId) return core.info('no matching asana short id in: ' + PR.title)
+    const task = await asana.getMatchingAsanaTask(ASANA_TOKEN, WORKSPACE, shortId)
+    core.info('got matching task: ' + JSON.stringify(task))
+  } catch (err) {
+    core.error(error.message)
   }
-  
-  const shortId = asana.getAsanaShortId(PR.title)
-  if (!shortId) return core.info('no matching asana short id in: ' + PR.title)
-  const task = await asana.getMatchingAsanaTask(ASANA_TOKEN, WORKSPACE, shortId)
-  core.info('got matching task: ' + JSON.stringify(task))
-} catch (error) {
-  core.error(error.message);
 }
+
+run()
