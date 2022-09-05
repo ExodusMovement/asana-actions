47acc740655c8cd956e7ddc91a6f9e431f4f849f
Federico Ponce de Leon <federico.po@exodus.io>
2022-09-05 14:47:15 -0300

    tests: mock fetch

--- src/__mocks__/fetch.js
+++ src/__mocks__/fetch.js
@@ -1,5 +1,5 @@
-const fixture = require('../tests/fixtures/asana.json')
-const projectsFixture = require('../tests/fixtures/asana-projects.json')
+const fixture = require('../../tests/fixtures/asana.json')
+const projectsFixture = require('../../tests/fixtures/asana-projects.json')
 
 const getTaskFromURL = (url) => {
   const taskId = /[0-9]+/.exec(url)[0]
